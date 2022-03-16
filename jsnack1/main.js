764baa4c9d09c5dcdfcb6bceaae4a1fee84b9254
dibss <ludovicad01@gmail.com>
2022-03-16 12:22:06 +0100

    .

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -1,18 +1,3 @@
-// const num1 = document.getElementById("num1").value;
-// const num2 = document.getElementById("num2").value;
-
-// button.addEventListener("click",
-//   function numeroMaggiore(){
-//     if (num1 > num2) {
-//       document.getElementById("num").innerHTML = num1;
-//     } else if (num1 == num2) {
-//       document.getElementById("num").innerHTML = "Pari";
-//     } else {
-//       document.getElementById("num").innerHTML = num2;
-//     }
-//   }
-// )
-
 const num1 = prompt("Inserisci il primo numero");
 const num2 = prompt("Inserisci il secondo numero");
 

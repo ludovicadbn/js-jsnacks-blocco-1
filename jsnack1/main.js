4022104a2948b592e266062e0a214850796c91d5
dibss <ludovicad01@gmail.com>
2022-03-16 13:18:33 +0100

    container

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -1,10 +1,10 @@
-const num1 = prompt("Inserisci il primo numero");
-const num2 = prompt("Inserisci il secondo numero");
+const num1 = Number(prompt("Inserisci il primo numero"));
+const num2 = Number(prompt("Inserisci il secondo numero"));
 
 if (num1 > num2) {
-  document.getElementById("num").innerHTML = num1;
+  document.getElementById("num").innerHTML = `${num1} è più grande di ${num2}`;
 } else if (num1 == num2) {
-  document.getElementById("num").innerHTML = "Pari";
+  document.getElementById("num").innerHTML = "I numeri sono uguali";
 } else {
-  document.getElementById("num").innerHTML = num2;
+  document.getElementById("num").innerHTML = `${num2} è più grande di ${num1}`;
 }

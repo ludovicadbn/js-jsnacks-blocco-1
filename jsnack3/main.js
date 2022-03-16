4cc3264a7028c6b1fa496c28fad90c086e8bb519
dibss <ludovicad01@gmail.com>
2022-03-16 13:07:40 +0100

    jsnack3

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,10 +1,7 @@
-// Il software deve chiedere per 10 volte all’utente di inserire un numero.
-// Il programma stampa la somma di tutti i numeri inseriti.
-
-let num = Number(prompt("Inserisci un numero da moltiplicare per 10"));
 let result = 0;
 
 for (i = 0; i < 10; i++) {
+  let num = Number(prompt("Inserisci un numero"));
   result += num;
 }
 
